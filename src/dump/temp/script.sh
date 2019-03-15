6017f0ba51859b5a2684c913a0fc81da03e7ead4
neeratyoy <neeratyoy@gmail.com>
2019-03-15 14:06:03 +0100

    Cleaning repo of experiment logs

--- src/dump/temp/script.sh
+++
@@ -1,5 +0,0 @@
-python3 sample.py --n_iterations 20 --eta 2 --min_budget 5 --max_budget 20 --out_dir bohb/KMNIST/2_5_20
-
-python3 sample.py --n_iterations 20 --eta 2 --min_budget 2 --max_budget 20 --out_dir bohb/K49/2_2_20 --dataset K49
-
-python3 sample.py --n_iterations 20 --eta 4 --min_budget 5 --max_budget 20 --out_dir bohb/K49/4_5_20 --dataset K49
